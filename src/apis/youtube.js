48b556ae139110b60822a392a2fa9e7f79760204
Sandali Weththasinghe <37362261+SandaliWeththasinghe@users.noreply.github.com>
2020-08-10 14:05:32 +0530

    axios added

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyBpMvCpG_hMNntvzCMh_gLGoB7p_19MJFs';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxresult: 5,
+    key: KEY
+  }
+});
