bb7ea09c3d45ad6812b1608b05e916a3a1eaee15
Sandali Weththasinghe <37362261+SandaliWeththasinghe@users.noreply.github.com>
2020-08-29 23:41:04 +0530

    Video player displaying done

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -3,9 +3,15 @@ import React from 'react';
 const VideoDetail = ({ video }) => {
   if(!video) {
   return <div>Loading...</div>
-  };
+  }
+ 
+  const videoScr = `https:/www.youtube.com/embed/${video.id.videoId}`;
+
   return (
     <div>
+      <div className="ui embed">
+        <iframe src={videoScr} />
+      </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
         <p>{video.snippet.description}</p>
